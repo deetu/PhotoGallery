ee2255a03e0a08a96cb9a833fc1758996d7409f2
Bada Adedayo <deetu02@gmail.com>
2016-11-18 17:22:28 +0000

    Removed commented code from PhotoGalleryActivity

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -16,10 +16,5 @@ public class PhotoGalleryActivity extends SingleFragmentActivity {
     protected Fragment createFragment() {
         return PhotoGalleryFragment.newInstance();
     }
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.fragment_photo_gallery);
-//    }
+    
 }
