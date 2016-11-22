a0370ad4aef64296f308287380d42f51f553b2a1
Bada Adedayo <deetu02@gmail.com>
2016-11-22 14:47:10 +0000

    Revert "reverted deleted files"
    This reverts commit c07904c9a5c6658317b57393c34920ff696a2bda.

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -3,6 +3,8 @@ package com.bignerdranch.android.photogallery;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
 
